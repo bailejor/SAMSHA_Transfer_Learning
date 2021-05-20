47d036b60e7e94a1f1893bc35b241ed372c1c5dc
Jordan Bailey <bailejor11@gmail.com>
2021-05-19 22:18:56 -0500

    Updated file to include PR curve

--- General_Substance_RandomForest.py
+++ General_Substance_RandomForest.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.metrics import roc_auc_score, balanced_accuracy_score, accuracy_score, make_scorer, precision_score, recall_score, confusion_matrix
+from sklearn.metrics import roc_auc_score, balanced_accuracy_score, accuracy_score, make_scorer, precision_score, recall_score, confusion_matrix, fbeta_score, precision_recall_curve
 from sklearn.ensemble import RandomForestClassifier
-import matplotlib.pyplot as plt
+from matplotlib import pyplot
 
 
 df = pd.read_csv('tedsd_puf_2018.csv', header  = 0)
@@ -47,10 +47,13 @@ data = df_methadone.values
 X = data2[:,1:29]
 y = data2[:,31:32].ravel()
 
-X_train, X_test30, y_train, y_test30 = train_test_split(X, y, test_size=0.30, random_state=33)
-X_val, X_test, y_val, y_test = train_test_split(X_test30, y_test30, test_size=0.50, random_state=33)
+X_train_70, X_test30, y_train_70, y_test30 = train_test_split(X, y, test_size=0.30, random_state=33)
+X_train, X_val, y_train, y_val = train_test_split(X_test30, y_test30, test_size=0.50, random_state=33)
 
-print(X_test.shape)
+print(len(X_train_70))
+
+print(len(X_val))
+
 
 param_grid = {'bootstrap':[True, False],
 'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
@@ -63,23 +66,27 @@ param_grid = {'bootstrap':[True, False],
 roc_auc = make_scorer(roc_auc_score)
 balanced = make_scorer(balanced_accuracy_score)
 acc = make_scorer(accuracy_score)
+fbeta = make_scorer(fbeta_score, beta = 0.5)
+
 
 rf = RandomForestClassifier(random_state= 33)
 
-clf = RandomizedSearchCV(rf, param_grid, n_iter = 10, scoring = roc_auc, n_jobs = 4, cv = 3, random_state = 33)
-search = clf.fit(X_train, y_train)
+clf = RandomizedSearchCV(rf, param_grid, n_iter = 10, scoring = fbeta, n_jobs = 4, cv = 3, random_state = 33)
+search = clf.fit(X_train_70, y_train_70)
 print(search.best_params_)
 print(search.best_score_)
 
 
 rf2 = RandomForestClassifier(**search.best_params_)
-rf2.fit(X_train, y_train)
+rf2.fit(X_train_70, y_train_70)
+
+
 
 yhat = rf2.predict(X_val)
 
 tn, fp, fn, tp = confusion_matrix(y_val, yhat).ravel()
 
-score1 = roc_auc_score(y_val, yhat)
+score1 = fbeta_score(y_val, yhat, beta = 0.5)
 score2 = balanced_accuracy_score(y_val, yhat)
 score3 = accuracy_score(y_val, yhat)
 
@@ -93,7 +100,7 @@ print(tn, fp, fn, tp)
 
 print(y_val)
 print(yhat)
-print("The ROC_AUC score is: " + str(score1))
+print("The F-Beta score is: " + str(score1))
 print("The Balanced Accuracy Score is: " + str(score2))
 print("The Accuracy Score is: " + str(score3))
 
@@ -103,9 +110,12 @@ print("The negative precision is: " + str(neg_precision))
 print("The negative recall is: " + str(neg_recall))
 
 
+rf_thresh = rf2.predict_proba(X_val)[:, 1].flatten()
+print(rf_thresh.shape)
+precision, recall, thresholds = precision_recall_curve(y_val, rf_thresh)
 
-
-
-
-
+pyplot.plot(recall, precision, marker='.', label='Base CNN', color = 'k')
+pyplot.xlabel('Recall')
+pyplot.ylabel('Precision')
+pyplot.show()
 
